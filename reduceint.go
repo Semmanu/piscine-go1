76f44f7e0bfbc63fb1e2da6947604d062fb5e44c
Semmanu <117686138+Semmanu@users.noreply.github.com>
2022-11-11 09:54:01 +0000

    Update reduceint.go

--- reduceint.go
+++ reduceint.go
@@ -1,27 +1,23 @@
-package main
+package piscine
 
-// The function should have as parameters a slice of integers, a []int and a function f func(int, int) int. You should apply for each element of the slice the arithmetic function, saving it and printing.
+import (
+	"strconv"
+
+	"github.com/01-edu/z01"
+)
 
 func ReduceInt(a []int, f func(int, int) int) {
-	result := a[0]
+	res := a[0]
 	for i := 1; i < len(a); i++ {
-		result = f(result, a[i])
+		res = f(res, a[i])
 	}
-	println(result)
+	num := strconv.Itoa(res)
+	PrintRune(num)
+	z01.PrintRune('\n')
 }
 
-// func main() {
-// 	mul := func(acc int, cur int) int {
-// 		return acc * cur
-// 	}
-// 	sum := func(acc int, cur int) int {
-// 		return acc + cur
-// 	}
-// 	div := func(acc int, cur int) int {
-// 		return acc / cur
-// 	}
-// 	as := []int{500, 2}
-// 	ReduceInt(as, mul)
-// 	ReduceInt(as, sum)
-// 	ReduceInt(as, div)
-// }
+func PrintRune(str string) {
+	for _, v := range str {
+		z01.PrintRune(v)
+	}
+}
